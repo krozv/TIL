2af2dcc02fc26686232a935c435dbfed8cba1850
krozv <krozv.code@gmail.com>
2024-06-16 22:12:58 +0900

    docs: 02-data_types.dart 내용 추가

--- 07-Dart/Basic/02-data_types.dart
+++ 07-Dart/Basic/02-data_types.dart
@@ -1,3 +1,35 @@
 void main() {
 
+  // 2.0 Basic Data Types
+  String name = "krozv";
+  bool alive = true;
+  int age = 12;
+  double money = 69.99;
+  num x = 12; // integer와 double의 부모 class
+  x = 3.4;
+
+  // 2.1 Lists
+  var giveMeFive = true;
+  var numbers = [
+    1, 
+    2, 
+    3, 
+    4,
+    if (giveMeFive) 5, // collection if
+    ];
+  List<int> nums = [1, 2, 3, 4,];
+  numbers.add(6);
+  print(numbers);
+  print(numbers.first);
+  print(numbers.last);
+  print(numbers.length);
+  
+  // 2.2 String Interpolation
+  var name2 = "krozv";
+  var age2 = 26;
+  var greeting = "Hello everyone, my name is $name2 and I'm ${age2 + 2}.";
+  print(greeting);
+
+  // 2.3 Collection For
+  
 }
