5a8a157897be924265cde27c75521c70ad53482a
krozv <krozv.code@gmail.com>
2024-06-18 01:15:25 +0900

    upload

---
+++ 09-C/240618/switch.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(){
+    int score = 101;
+    switch(score/10){
+        case 10:
+        case 9:
+            printf("A"); break;
+        case 8:
+            printf("B");
+            break;
+        default:
+            printf("F");
+    }
+    return 0;
+}
+// 출력
+// A
